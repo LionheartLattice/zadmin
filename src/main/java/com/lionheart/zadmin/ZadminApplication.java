ce80753a8753c1bc2414857a9606267e7aa5c2d8
lionheart <2451809708@qq.com>
2025-12-06 06:42:46 +0800

    初始化项目

---
+++ src/main/java/com/lionheart/zadmin/ZadminApplication.java
@@ -0,0 +1,13 @@
+package com.lionheart.zadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lionheart/zadmin/ZadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionheart.zadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
