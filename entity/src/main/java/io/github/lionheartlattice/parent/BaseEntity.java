43b26f2a38a4e80d4c894029f008f11a31d60047
lionheart <2451809708@qq.com>
2025-12-07 19:40:33 +0800

    refactor(entity): 实体类重构以支持代理实体
    - BaseEntity 类增加泛型参数以支持代理实体
    - UserEntity 类继承修改后的 BaseEntity
    - 优化异常处理，使用 CloneRuntimeException 替换 RuntimeException

--- entity/src/main/java/io/github/lionheartlattice/parent/BaseEntity.java
+++ entity/src/main/java/io/github/lionheartlattice/parent/BaseEntity.java
@@ -2,15 +2,18 @@ package io.github.lionheartlattice.parent;
 
 import cn.hutool.core.clone.CloneRuntimeException;
 import cn.hutool.core.clone.Cloneable;
+import com.easy.query.core.proxy.ProxyEntity;
+import com.easy.query.core.proxy.ProxyEntityAvailable;
 
 import java.io.*;
 
 /**
  * 实体基类，提供通用功能
  *
- * @param <T> 实体类型
+ * @param <T>      实体类型
+ * @param <TProxy> 对应的代理实体类型
  */
-public class BaseEntity<T> implements Cloneable<T>, Serializable {
+public class BaseEntity<T extends BaseEntity<T, TProxy> & ProxyEntityAvailable<T, TProxy>, TProxy extends ProxyEntity<TProxy, T>> implements Cloneable<T>, Serializable, ProxyEntityAvailable<T, TProxy> {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -27,24 +30,23 @@ public class BaseEntity<T> implements Cloneable<T>, Serializable {
 
     /**
      * 通过序列化实现深克隆
+     *
      * @return 深克隆后的对象
      */
     @SuppressWarnings("unchecked")
     public T deepClone() {
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); ObjectOutputStream oos = new ObjectOutputStream(bos)) {
 
             // 序列化
             oos.writeObject(this);
 
-            try (ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
-                 ObjectInputStream ois = new ObjectInputStream(bis)) {
+            try (ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray()); ObjectInputStream ois = new ObjectInputStream(bis)) {
 
                 // 反序列化
                 return (T) ois.readObject();
             }
         } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException("深克隆失败", e);
+            throw new CloneRuntimeException(e);
         }
     }
 }
