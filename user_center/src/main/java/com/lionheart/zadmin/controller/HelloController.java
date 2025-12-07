7c328d181f0f2b3121a98071e79f01d39c4b84ed
lionheart <2451809708@qq.com>
2025-12-07 11:04:04 +0800

    refactor(user_center): 重构用户中心相关代码
    - 删除 Company 类和 UserEntityNew 类
    - 更新 HelloController 中的数据库查询语句和实体类
    - 删除测试文本文件

--- user_center/src/main/java/com/lionheart/zadmin/controller/HelloController.java
+++ user_center/src/main/java/com/lionheart/zadmin/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.lionheart.zadmin.controller;
 
+import com.lionheart.zadmin.user_center.po.UserEntity;
 import com.lionheart.zadmin.util.parent.ParentController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,30 +27,30 @@ public class HelloController extends ParentController {
     @Operation(summary = "获取所有用户(主库-JDBC)", description = "使用JdbcTemplate查询主库z_user表")
     @GetMapping("/user")
     public List<Map<String, Object>> getUser() {
-        return jdbcTemplate.queryForList("select * from t_user");
+        return jdbcTemplate.queryForList("select * from z_user");
     }
 
     // 使用主数据源 EasyQuery 查询
     @Operation(summary = "查询小米公司(主库-EQ)", description = "使用EasyQuery查询主库")
     @GetMapping("/say")
-    public List<Company> say() {
-        return entityQuery.queryable(Company.class).where(c -> c.name().like("小米"))
-                .toList();
+    public List<UserEntity> say() {
+        return entityQuery.queryable(UserEntity.class).where(c -> c.nickname()
+                .like("张")).toList();
     }
 
     // 使用 ds2 数据源 JdbcTemplate 查询
     @Operation(summary = "获取所有用户(DS2-JDBC)", description = "使用JdbcTemplate查询DS2库")
     @GetMapping("/ds2/user")
     public List<Map<String, Object>> getDs2User() {
-        return ds2JdbcTemplate.queryForList("select * from t_user");
+        return ds2JdbcTemplate.queryForList("select * from z_user");
     }
 
     // 使用 ds2 数据源 EasyQuery 查询
     @Operation(summary = "查询小米公司(DS2-EQ)", description = "使用EasyQuery查询DS2库")
     // 使用 ds2 数据源 EasyQuery 查询
     @GetMapping("/ds2/say")
-    public List<Company> ds2Say() {
-        return ds2EntityQuery.queryable(Company.class).where(c -> c.name().like("小米"))
-                .toList();
+    public List<UserEntity> ds2Say() {
+        return ds2EntityQuery.queryable(UserEntity.class).where(c -> c.nickname()
+                .like("张")).toList();
     }
 }
