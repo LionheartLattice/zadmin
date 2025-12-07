3b0d9bfba9dd71f944fc96bf8f65833928567473
lionheart <2451809708@qq.com>
2025-12-07 18:29:57 +0800

    refactor(entity): 创建 BaseEntity 类并使 UserEntity 继承它
    - 新增 BaseEntity 类，提供通用功能
    - 修改 UserEntity 类，使其继承 BaseEntity
    - 增加 @EqualsAndHashCode 注解，确保正确实现 equals 和 hashCode 方法

---
+++ entity/src/main/java/io/github/lionheartlattice/parent/BaseEntity.java
@@ -0,0 +1,11 @@
+package io.github.lionheartlattice.parent;
+
+import cn.hutool.core.clone.CloneSupport;
+
+/**
+ * 实体基类，提供通用功能
+ * @param <T> 实体类型
+ */
+public class BaseEntity<T> extends CloneSupport<T> {
+
+}
