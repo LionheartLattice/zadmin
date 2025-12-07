1f60ba89e0af9e098834e020cdb76efcef5b1c0c
lionheart <2451809708@qq.com>
2025-12-07 09:54:36 +0800

    feat(entity): 新增用户实体类 UserEntity 并配置相关依赖
    - 创建 `UserEntity` 类，定义系统用户实体及其字段注解
    - 在 `entity` 模块中增加 `spring-boot-starter-validation` 和 `knife4j-openapi3` 依赖
    - 更新 easy-query.setting 文件，指定 `UserEntity` 的生成配置

---
+++ entity/src/main/java/com/lionheart/zadmin/user_center/po/UserEntity.java
@@ -0,0 +1,119 @@
+package com.lionheart.zadmin.user_center.po;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import io.swagger.v3.oas.annotations.media.Schema;
+import com.easy.query.core.annotation.Column;
+import com.easy.query.core.annotation.Table;
+import com.easy.query.core.annotation.EntityProxy;
+import com.easy.query.core.annotation.EasyAssertMessage;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * 系统用户表 实体类。
+ *
+ * @author lionheart
+ * @since 1.0
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "系统用户表")
+@Table(value = "z_user")
+@EntityProxy
+@EasyAssertMessage("未找到对应的系统用户表信息")
+public class UserEntity {
+
+
+    /**
+     * 用户ID
+     */
+    @Schema(description = "用户ID", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Column(primaryKey = true, value = "id")
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    @Schema(description = "用户名", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String username;
+
+    /**
+     * 密码
+     */
+    @Schema(description = "密码", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String pwd;
+
+    /**
+     * 手机号
+     */
+    @Schema(description = "手机号")
+    private String phone;
+
+    /**
+     * 昵称
+     */
+    @Schema(description = "昵称")
+    private String nickname;
+
+    /**
+     * 性别 未知-男-女
+     */
+    @Schema(description = "性别 未知-男-女")
+    private String sex;
+
+    /**
+     * 生日
+     */
+    @Schema(description = "生日")
+    private LocalDate birthday;
+
+    /**
+     * 头像base64
+     */
+    @Schema(description = "头像base64")
+    private String logo;
+
+    /**
+     * 身份证
+     */
+    @Schema(description = "身份证")
+    private String idCard;
+
+    /**
+     * 邮箱地址
+     */
+    @Schema(description = "邮箱地址")
+    private String email;
+
+    /**
+     * 更新时间
+     */
+    @Schema(description = "更新时间", example = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+
+    /**
+     * 是否删除
+     */
+    @Schema(description = "是否删除")
+    private Boolean delFlag;
+
+    /**
+     * 创建人ID
+     */
+    @Schema(description = "创建人ID")
+    private Long createId;
+
+    /**
+     * 更新人ID
+     */
+    @Schema(description = "更新人ID")
+    private Long updateId;
+
+
+}
