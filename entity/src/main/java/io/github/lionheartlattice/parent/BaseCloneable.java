2f11229c1249801700740a6456793389f4550eac
lionheart <2451809708@qq.com>
2025-12-08 08:45:45 +0800

    refactor(entity): 重构 BaseCloneable 类
    - 移除了 Hutool 工具类的依赖，改用 Spring 的 BeanUtils
    - 删除了不必要的注释
    - 优化了 copyTo 方法，增加了异常处理
    - 调整了访问修饰符，将 entityClass 方法公开

--- entity/src/main/java/io/github/lionheartlattice/parent/BaseCloneable.java
+++ entity/src/main/java/io/github/lionheartlattice/parent/BaseCloneable.java
@@ -1,31 +1,16 @@
 package io.github.lionheartlattice.parent;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.clone.CloneRuntimeException;
 import cn.hutool.core.clone.Cloneable;
+import org.springframework.beans.BeanUtils;
 
 import java.io.*;
 
-/**
- * 基础克隆类，提供克隆和序列化功能
- *
- * @param <T> 实体类型
- */
 public abstract class BaseCloneable<T> implements Cloneable<T>, Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private static final CopyOptions DEFAULT_COPY_OPTIONS = CopyOptions.create()
-            .setIgnoreNullValue(true).setIgnoreError(true);
-
-    /**
-     * 通过序列化实现对象深克隆
-     *
-     * @param obj 待克隆对象
-     * @return 深克隆后的对象
-     */
     private static Object deepCloneObject(Object obj) {
         try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); ObjectOutputStream oos = new ObjectOutputStream(bos)) {
             oos.writeObject(obj);
@@ -47,61 +32,43 @@ public abstract class BaseCloneable<T> implements Cloneable<T>, Serializable {
         }
     }
 
-    /**
-     * 通过序列化实现深克隆
-     *
-     * @return 深克隆后的对象
-     */
     @SuppressWarnings("unchecked")
     public T deepClone() {
         return (T) deepCloneObject(this);
     }
 
-    /**
-     * 从源对象深拷贝属性到当前对象（忽略null值）
-     * 先深克隆源对象，再复制属性
-     *
-     * @param source 源对象
-     * @return 当前对象
-     */
     @SuppressWarnings("unchecked")
     public T copyFrom(Object source) {
         if (source != null) {
             Object clonedSource = source instanceof Serializable ? deepCloneObject(source) : source;
-            BeanUtil.copyProperties(clonedSource, this, DEFAULT_COPY_OPTIONS);
+            BeanUtils.copyProperties(clonedSource, this);
         }
         return (T) this;
     }
 
-    /**
-     * 将当前对象深拷贝属性到目标对象（忽略null值）
-     *
-     * @param target 目标对象
-     * @param <R>    目标类型
-     * @return 目标对象
-     */
     public <R> R copyTo(R target) {
         if (target != null) {
             Object clonedThis = deepCloneObject(this);
-            BeanUtil.copyProperties(clonedThis, target, DEFAULT_COPY_OPTIONS);
+            BeanUtils.copyProperties(clonedThis, target);
         }
         return target;
     }
 
-    /**
-     * 将当前对象深拷贝转换为指定类型的新对象
-     *
-     * @param targetClass 目标类型
-     * @param <R>         目标类型
-     * @return 新创建的目标对象
-     */
     public <R> R copyTo(Class<R> targetClass) {
-        Object clonedThis = deepCloneObject(this);
-        return BeanUtil.copyProperties(clonedThis, targetClass);
+        try {
+            R target = targetClass.getDeclaredConstructor().newInstance();
+            Object clonedThis = deepCloneObject(this);
+            BeanUtils.copyProperties(clonedThis, target);
+            return target;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create instance", e);
+        }
     }
 
     @SuppressWarnings("unchecked")
-    protected Class<T> entityClass() {
+    public Class<T> entityClass() {
         return (Class<T>) getClass();
     }
+
+
 }
