b4a8625bee437a22a765b58ac23ca9cebb8da2f4
lionheart <2451809708@qq.com>
2025-12-08 09:36:08 +0800

    refactor(entity): 重构复制方法实现浅拷贝和深拷贝的区分
    - 修改 BaseCloneable 类中的复制方法，使用 CopyUtil.copy 替代 CopyUtil.deepCopy
    - 重构 CopyUtil 类，增加浅拷贝方法和新的复制逻辑
    - 优化对象复制工具类，明确区分深拷贝和浅拷贝功能

--- base/src/main/java/io/github/lionheartlattice/configuration/util/CopyUtil.java
+++ base/src/main/java/io/github/lionheartlattice/configuration/util/CopyUtil.java
@@ -1,105 +1,117 @@
 package io.github.lionheartlattice.configuration.util;
 
-import cn.hutool.core.clone.CloneRuntimeException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.BeanUtils;
-import org.springframework.util.Assert;
+     import cn.hutool.core.clone.CloneRuntimeException;
+     import com.fasterxml.jackson.databind.ObjectMapper;
+     import org.springframework.beans.BeanUtils;
+     import org.springframework.util.Assert;
 
-import java.io.*;
-import java.lang.reflect.Constructor;
+     import java.io.*;
+     import java.lang.reflect.Constructor;
 
-/**
- * 对象复制工具类
- */
-public abstract class CopyUtil {
+     /**
+      * 对象复制工具类
+      * <p>默认方法为深拷贝，浅拷贝使用 shallow 前缀
+      */
+     public abstract class CopyUtil {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+         private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private CopyUtil() {
-    }
+         private CopyUtil() {
+         }
 
-    /**
-     * 浅拷贝属性到新实例
-     */
-    public static <T> T copy(Object source, Class<T> targetClass) {
-        Assert.notNull(source, "Source must not be null");
-        Assert.notNull(targetClass, "Target class must not be null");
-        T target = instantiate(targetClass);
-        BeanUtils.copyProperties(source, target);
-        return target;
-    }
+         /**
+          * 深拷贝到新实例 - 自动选择策略
+          */
+         @SuppressWarnings("unchecked")
+         public static <T> T copy(T source) {
+             Assert.notNull(source, "Source must not be null");
+             if (source instanceof Serializable) {
+                 return (T) copyBySerializable((Serializable) source);
+             }
+             return copyByJackson(source, (Class<T>) source.getClass());
+         }
 
-    /**
-     * 浅拷贝属性到已有实例
-     */
-    public static <T> T copy(Object source, T target) {
-        Assert.notNull(source, "Source must not be null");
-        Assert.notNull(target, "Target must not be null");
-        BeanUtils.copyProperties(source, target);
-        return target;
-    }
+         /**
+          * 深拷贝到指定类型的新实例
+          */
+         public static <T> T copy(Object source, Class<T> targetClass) {
+             Assert.notNull(source, "Source must not be null");
+             Assert.notNull(targetClass, "Target class must not be null");
+             return copyByJackson(source, targetClass);
+         }
 
-    /**
-     * 深拷贝 - 自动选择策略
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T deepCopy(T source) {
-        Assert.notNull(source, "Source must not be null");
-        if (source instanceof Serializable) {
-            return (T) deepCopyBySerializable((Serializable) source);
-        }
-        return deepCopyByJackson(source, (Class<T>) source.getClass());
-    }
+         /**
+          * 深拷贝到已有实例
+          */
+         public static <T> T copy(Object source, T target) {
+             Assert.notNull(source, "Source must not be null");
+             Assert.notNull(target, "Target must not be null");
+             T deepSource = copy(source);
+             BeanUtils.copyProperties(deepSource, target);
+             return target;
+         }
 
-    /**
-     * 深拷贝到指定类型
-     */
-    public static <T> T deepCopy(Object source, Class<T> targetClass) {
-        Assert.notNull(source, "Source must not be null");
-        Assert.notNull(targetClass, "Target class must not be null");
-        return deepCopyByJackson(source, targetClass);
-    }
+         /**
+          * 通过序列化深拷贝
+          */
+         @SuppressWarnings("unchecked")
+         public static <T extends Serializable> T copyBySerializable(T source) {
+             try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                  ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+                 oos.writeObject(source);
+                 try (ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+                      ObjectInputStream ois = new ObjectInputStream(bis)) {
+                     return (T) ois.readObject();
+                 }
+             } catch (IOException | ClassNotFoundException e) {
+                 throw new CloneRuntimeException(e);
+             }
+         }
 
-    /**
-     * 通过序列化深拷贝
-     */
-    @SuppressWarnings("unchecked")
-    public static <T extends Serializable> T deepCopyBySerializable(T source) {
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
-            oos.writeObject(source);
-            try (ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
-                 ObjectInputStream ois = new ObjectInputStream(bis)) {
-                return (T) ois.readObject();
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            throw new CloneRuntimeException(e);
-        }
-    }
+         /**
+          * 通过 Jackson 深拷贝
+          */
+         public static <T> T copyByJackson(Object source, Class<T> targetClass) {
+             try {
+                 String json = OBJECT_MAPPER.writeValueAsString(source);
+                 return OBJECT_MAPPER.readValue(json, targetClass);
+             } catch (Exception e) {
+                 throw new CloneRuntimeException(e);
+             }
+         }
 
-    /**
-     * 通过 Jackson 深拷贝
-     */
-    public static <T> T deepCopyByJackson(Object source, Class<T> targetClass) {
-        try {
-            String json = OBJECT_MAPPER.writeValueAsString(source);
-            return OBJECT_MAPPER.readValue(json, targetClass);
-        } catch (Exception e) {
-            throw new CloneRuntimeException(e);
-        }
-    }
+         /**
+          * 浅拷贝属性到新实例
+          */
+         public static <T> T shallowCopy(Object source, Class<T> targetClass) {
+             Assert.notNull(source, "Source must not be null");
+             Assert.notNull(targetClass, "Target class must not be null");
+             T target = instantiate(targetClass);
+             BeanUtils.copyProperties(source, target);
+             return target;
+         }
 
-    /**
-     * 实例化对象
-     */
-    public static <T> T instantiate(Class<T> clazz) {
-        Assert.notNull(clazz, "Class must not be null");
-        try {
-            Constructor<T> constructor = clazz.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to instantiate: " + clazz.getName(), e);
-        }
-    }
-}
+         /**
+          * 浅拷贝属性到已有实例
+          */
+         public static <T> T shallowCopy(Object source, T target) {
+             Assert.notNull(source, "Source must not be null");
+             Assert.notNull(target, "Target must not be null");
+             BeanUtils.copyProperties(source, target);
+             return target;
+         }
+
+         /**
+          * 实例化对象
+          */
+         public static <T> T instantiate(Class<T> clazz) {
+             Assert.notNull(clazz, "Class must not be null");
+             try {
+                 Constructor<T> constructor = clazz.getDeclaredConstructor();
+                 constructor.setAccessible(true);
+                 return constructor.newInstance();
+             } catch (Exception e) {
+                 throw new RuntimeException("Failed to instantiate: " + clazz.getName(), e);
+             }
+         }
+     }
