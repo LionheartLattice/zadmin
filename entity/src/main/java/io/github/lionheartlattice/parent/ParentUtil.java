1068f830a1649cdb416ac2b8bfb35c51f60f36dc
lionheart <2451809708@qq.com>
2025-12-08 18:11:23 +0800

    feat(util): 添加 JdbcTemplate 和 EasyEntityQuery 支持以增强数据源操作能力

--- entity/src/main/java/io/github/lionheartlattice/parent/ParentUtil.java
+++ entity/src/main/java/io/github/lionheartlattice/parent/ParentUtil.java
@@ -1,9 +1,13 @@
 package io.github.lionheartlattice.parent;
 
+import com.easy.query.api.proxy.client.EasyEntityQuery;
+import io.github.lionheartlattice.configuration.easyquery.DataAccessUtils;
 import io.github.lionheartlattice.util.NullUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ResolvableType;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * 父级工具类，提供实体类操作的通用方法
@@ -15,9 +19,35 @@ import org.springframework.core.ResolvableType;
 public abstract class ParentUtil<T> extends NullUtil {
 
     /**
+     *
      * 日志实例，使用实际子类的 Class
      */
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 主数据源 JdbcTemplate
+     */
+    protected final JdbcTemplate jdbcTemplate = DataAccessUtils.getJdbcTemplate();
+
+    /**
+     * 第二数据源 JdbcTemplate
+     */
+    protected final JdbcTemplate ds2JdbcTemplate = DataAccessUtils.getDs2JdbcTemplate();
+
+    /**
+     * 主数据源 EasyEntityQuery
+     */
+    protected final EasyEntityQuery entityQuery = DataAccessUtils.getEntityQuery();
+
+    /**
+     * 第二数据源 EasyEntityQuery
+     */
+    protected final EasyEntityQuery ds2EntityQuery = DataAccessUtils.getDs2EntityQuery();
+
+    /**
+     * StringRedisTemplate
+     */
+    protected final StringRedisTemplate redisTemplate = DataAccessUtils.getStringRedisTemplate();
 
     /**
      * 获取实体类的泛型类型
