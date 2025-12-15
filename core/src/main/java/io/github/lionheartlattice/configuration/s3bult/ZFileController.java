77d89778b5004c2fb2e755638f34ce3c94ea41a2
lionheart <2451809708@qq.com>
2025-12-15 18:41:15 +0800

    feat(oss): 为文件上传新增用途参数支持，更新控制器接口及服务方法调用

--- core/src/main/java/io/github/lionheartlattice/configuration/s3bult/ZFileController.java
+++ core/src/main/java/io/github/lionheartlattice/configuration/s3bult/ZFileController.java
@@ -1,6 +1,5 @@
 package io.github.lionheartlattice.configuration.s3bult;
 
-import io.github.lionheartlattice.entity.parent.ZFile;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,14 +25,16 @@ public class ZFileController {
     /**
      * 上传文件并返回URL
      *
-     * @param file 文件对象
+     * @param file  文件对象
+     * @param usage 用途
      * @return 文件访问链接
      */
     @PostMapping(value = "/upload-url", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "上传文件返回URL", description = "上传文件并直接返回可访问的HTTP链接")
     public String uploadReturnUrl(
-            @Parameter(description = "文件", required = true) @RequestPart("file") MultipartFile file) {
-        return zFileService.uploadReturnUrl(file,usage);
+            @Parameter(description = "文件", required = true) @RequestPart("file") MultipartFile file,
+            @Parameter(description = "用途", required = false) @RequestParam(required = false) String usage) {
+        return zFileService.uploadReturnUrl(file, usage);
     }
 
     /**
