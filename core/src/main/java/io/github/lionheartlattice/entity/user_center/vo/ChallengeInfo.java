629d6c38326d64877bc2b4bdcdcdba15285f00ba
lionheart <2451809708@qq.com>
2025-12-15 14:10:34 +0800

    feat(user_center): 新增 ChallengeInfo VO 类，定义请求标识和秘钥字段

---
+++ core/src/main/java/io/github/lionheartlattice/entity/user_center/vo/ChallengeInfo.java
@@ -0,0 +1,10 @@
+package io.github.lionheartlattice.entity.user_center.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class ChallengeInfo {
+    @Schema(description = "临时请求标识")
+    private String requestId;
+    @Schema(description = "秘钥")
+    private String secretKey;
+}
